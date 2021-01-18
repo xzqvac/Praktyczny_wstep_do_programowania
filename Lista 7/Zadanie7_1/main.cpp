f79c137d13c45f036a491caa81d4ce25c8b9342c
xzqvac <kawkaradoslaw@gmail.com>
2021-01-18 02:13:12 +0100

    pop

--- Lista 7/Zadanie7_1/main.cpp
+++ Lista 7/Zadanie7_1/main.cpp
@@ -1,28 +1,84 @@
-#include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+
 int main()
 {
-    int keyonoff=1;
+    const int szerokosc = 800;
+    const int wysokosc = 800;
+    const int promien = 50;
+    /*float x = 50.0;
+    float y = 50.0;
+    float vx = 0.05;
+    float vy = 0.05;*/
+    float dt = 1.0;
 
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Nasze okno");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Red);
+    const int N = 3;
+    float x[3] = { 100.0, 100.0, 100.0 };
+    float y[3] = { 50.0, 23.0, 38.9 };
+    float vx[3] = { 0.005, 0.006, 0.012 };
+    float vy[3] = { 0.05, 0.06, 0.12 };
+    float g[3] = {10, 30, 50};
+
+    sf::RenderWindow window(sf::VideoMode(szerokosc, wysokosc), "SFML works!");
+    //sf::CircleShape shape(100.f);
+    //sf::CircleShape circle(promien);
+    //circle.setFillColor(sf::Color::Green);
+
+    std::vector <sf::CircleShape> shapes(N);
+    for (int i = 0; i < N; i++)
+    {
+        sf::CircleShape shape(promien);
+        shape.setFillColor(sf::Color::Green);
+        shapes[i] = shape;
+    }
 
     while (window.isOpen())
     {
-        sf::Event event;
-    while (window.pollEvent(event))
-    {
-        if(event.type == sf::Event::Closed)
-            window.close();
-        if(event.type == sf::Event::KeyPressed)
-            keyonoff=1-keyonoff;
-    }
-    window.clear(sf::Color::Black);
+        for (int i = 0; i < N; i++)
+        {
+            if (x[i] > szerokosc-(2*promien)) //szerokosc-2*(promien)
+            {
+                vx[i] = -vx[i];
+            }
+            else if (x[i] < 0)
+            {
+                vx[i] = -vx[i];
+            }
+            if (y[i] > wysokosc-(2*promien))
+            {
+                vy[i] = -vy[i];
+            }
+            else if (y[i] < 0)
+            {
+                //vy[i] = -vy[i];
+                y[i] += dt;
+                //vy[i] += g[i];
+            }
+            x[i] = x[i] + (vx[i] * dt);
+            y[i] = y[i] + (vy[i] * dt);
+            vy[i] = vy[i] + 0.001 * dt;
+            g[i] += vy[i];
+            //vx[i] = vx[i] + gravity * dt;
+            shapes[i].setFillColor(sf::Color(0 + g[i], 125, 0));
+        }
 
-    if(keyonoff)
-        window.draw(shape);
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+           if (event.type == sf::Event::Closed)
+                window.close();
+
+            else if (event.key.code == sf::Keyboard::Escape)
+                window.close();
+        }
+
+        window.clear();
+        for (int i = 0; i < N; i++)
+        {
+            window.draw(shapes[i]);
+            shapes[i].setPosition(x[i], y[i]);
+        }
         window.display();
     }
+
     return 0;
 }
