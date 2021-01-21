14f286ca3cad3b6e6fe6b9324c8c5792f00e6e4f
xzqvac <kawkaradoslaw@gmail.com>
2021-01-21 13:43:05 +0100

    Pop

--- Lista 4/Zadanie 5/main.cpp
+++ Lista 4/Zadanie 5/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <fstream>
+
 using namespace std;
 
-int fibb(int a)
+int fibonacci(int a)
 {
-    if(a==0)
+    if(a == 0)
     {
         return 0;
     }
@@ -13,19 +14,37 @@ int fibb(int a)
         return 1;
     }
     else
-        return fibb(a-2) + fibb(a-1);
+        return fibonacci(a-2) + fibonacci(a-1);
+}
+
+int liczbyGnomiczne(int b)
+{
+    return 2*b+1 + b*b;
 }
 
 int main()
 {
     int a;
-    ofstream wynik;
-    wynik.open("fib.dat.txt");
+    int b;
+    ofstream wynikfibonacci,wynikliczbyGnomiczne;
+    wynikfibonacci.open("fib.dat.txt");
+    wynikliczbyGnomiczne.open("lgn.dat.txt");
     cout << "Ile wyrazow ciagu Fibbonaciego chcesz zobaczyc?: "<<endl;
     cin>>a;
     for(int i=0;i<a;i++)
     {
-        cout<<"Numer wyrazu "<<i<<" :"<<fibb(i)<<endl;
-        wynik<<i<<" "<<fibb(i)<<endl;
+        cout<<"Numer wyrazu "<<i<<" :"<<fibonacci(i)<<endl;
+        wynikfibonacci<<i<<" "<<fibonacci(i)<<endl;
     }
+
+    cout << "Liczby gnomiczne to liczby postaci 2n+1, ktore dodane do kwadratu liczby n daja kwadrat nastepnej liczby"<<endl;
+    cout << "Ile wyrazow ciagu liczb gnomicznych chcesz zobaczyc?: "<<endl;
+    cin>>b;
+
+    for(int i=1;i<b;i++)
+    {
+        cout<<"Numer wyrazu "<<i<<" :"<<liczbyGnomiczne(i)<< endl;
+        wynikliczbyGnomiczne<<i<<" "<<liczbyGnomiczne(i)<<endl;
+    }
+
 }
